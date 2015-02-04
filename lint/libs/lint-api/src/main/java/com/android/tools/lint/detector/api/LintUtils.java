tadfisher/platform_tools_base

Add ItemResourceValue
The new class represents an item in the style resource. Earlier,
ResourceValue was used along with a boolean to represent the attribute
namespace.

Change-Id: I03344f7561952260d8155d6a39119b4b54d38285

lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/LintUtils.java
@@ -34,6 +34,7 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.model.AndroidProject;
 import com.android.builder.model.ApiVersion;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.ide.common.res2.AbstractResourceRepository;
@@ -860,7 +861,7 @@ public class LintUtils {
                     ResourceValue rv = item.getResourceValue(false);
                     if (rv instanceof StyleResourceValue) {
                         StyleResourceValue srv = (StyleResourceValue) rv;
-                        ResourceValue value = srv.findValue(attribute, isFrameworkAttribute);
+                        ItemResourceValue value = srv.findVal(attribute, isFrameworkAttribute);
                         if (value != null) {
                             if (result == null) {
                                 result = Lists.newArrayList();

glowingyilia/ddmlib

(cherry picked from commit c8e56213d1c45b7919f7ff7ba845335e77783565)

lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/LintUtils.java
@@ -33,6 +33,7 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.model.AndroidProject;
 import com.android.builder.model.ApiVersion;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.ide.common.res2.AbstractResourceRepository;
@@ -858,7 +859,7 @@ public class LintUtils {
                     ResourceValue rv = item.getResourceValue(false);
                     if (rv instanceof StyleResourceValue) {
                         StyleResourceValue srv = (StyleResourceValue) rv;
-                        ResourceValue value = srv.findValue(attribute, isFrameworkAttribute);
+                        ItemResourceValue value = srv.getItem(attribute, isFrameworkAttribute);
                         if (value != null) {
                             if (result == null) {
                                 result = Lists.newArrayList();